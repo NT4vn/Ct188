8db0d8a0e771a53057969d22416a3adaab7f900a
NT4vn <loc9410@gmail.com>
2025-01-14 22:23:10 +0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $(".banner-wrapper").slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: true,
+  });
+});
